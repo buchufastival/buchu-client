b3b41c365d0b71a41e7bbf16c6f88b5c18272dd0
sookyoungwoo <sookyoung2005@gmail.com>
2023-04-08 23:54:35 +0900

    ADD :: button 12월까지

--- src/components/Button/Month/Month.js
+++ src/components/Button/Month/Month.js
@@ -1,7 +1,17 @@
 export default function Month() {
   return (
     <>
-      <input type="radio" />
+      <input type="radio" /> 1월
+      <input type="radio" />2월
+      <input type="radio" />3월
+      <input type="radio" />4월
+      <input type="radio" />6월
+      <input type="radio" />7월
+      <input type="radio" />8월
+      <input type="radio" />9월
+      <input type="radio" />10월
+      <input type="radio" />11월
+      <input type="radio" />12월
     </>
   );
 }
