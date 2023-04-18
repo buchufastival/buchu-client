3a904cf71fb37153b00bde1a8d9a570e325f1410
sookyoungwoo <sookyoung2005@gmail.com>
2023-04-18 23:47:45 +0900

    ADD :: test data

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,8 +1,27 @@
-import './Detail.scss';
-export default function Detail(){
-    return(
-        <>
-
-        </>
-    )
+import "./Detail.scss";
+import data from "./data.json";
+export default function Detail() {
+  return(
+    <section>
+        <div>
+            <div>
+                <img src={data.MAIN_IMG_THUMB}/>
+            </div>
+            <div>
+                <div>
+                    <p>{data.MAIN_TITLE}</p>
+                    <p>{data.SUBTITLE}</p>
+                    <p>{data.ITEMCNTNTS}</p>
+                </div>
+                <div>
+                    <div>
+                        <p>장소</p>
+                        <p>{data.MAIN_PLACE}</p>
+                        <p>{data.ADDR1}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
 }
